40d4f47c5a1eccae304e09976378b3bb9ba1fde1
yuxuanjava <han630389657@vip.qq.com>
2019-12-03 18:46:59 +0800

    2019-12-03 18:46:53continuous_optimization

---
+++ src/main/java/club/yuxuan/yun/eureka/EurekaConfiguration.java
@@ -0,0 +1,16 @@
+package club.yuxuan.yun.eureka;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class EurekaConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+    
+}
